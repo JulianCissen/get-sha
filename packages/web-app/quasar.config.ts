9331fac610af7d4718a65e8bc86c7fdc23f45ffc
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2025-03-31 21:01:38 +0200

    ci: update base URL in Quasar config for github pages

--- packages/web-app/quasar.config.ts
+++ packages/web-app/quasar.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from '#q-app/wrappers';
 export default defineConfig((/* ctx */) => {
     return {
         // Hard coded base URL for github pages
-        base: 'https://juliancissen.github.io/get-sha/',
+        base: './',
 
         // https://v2.quasar.dev/quasar-cli-vite/prefetch-feature
         // preFetch: true,
